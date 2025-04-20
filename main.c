a908c5ae608ed1a97ea0a6eb517c6fa53d7a2dc3
VitulakCZ <stemberk.vit@gmail.com>
2025-04-20 10:08:09 +0200

    Argumenty a write

--- main.c
+++ main.c
@@ -3,14 +3,17 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #define BUF_SIZE 256
-#define FILE_SIZE 1e6
+#define ARGS_LIMIT 5
+#define FILE_SIZE 1e7
+#define ERROR printf("?\n")
 
 int main(int argc, char *argv[]) {
     bool append_mode = false;
-    int pocet_radku = 0;
+    int c;
     FILE *file;
     char *filename;
     char *file_buffer = malloc(sizeof(char)*FILE_SIZE);
+    int pocet_radku = 0;
     if (argc > 1) {
         if (argc >= 3) {
             printf("Too many arguments!\n");
@@ -18,24 +21,61 @@ int main(int argc, char *argv[]) {
         }
         filename = argv[1];
         file = fopen(filename, "r");
-        while (getc(file) != EOF) {
-            file_buffer[pocet_radku++] = getc(file);
+        while ((c = getc(file)) != EOF) {
+            file_buffer[pocet_radku++] = c;
         }
         fclose(file);
         printf("%d\n", pocet_radku);
     }
     char input[BUF_SIZE];
-    while (strcmp(input, "q")) {
+    while (true) {
         fgets(input, BUF_SIZE, stdin);
         input[strcspn(input, "\n")] = 0;
+        
         if (!append_mode) {
-            if (!strcmp(input, "a"))
-                append_mode = true;
-            else if (!strcmp(input, "p"))
-                printf(file_buffer);
-            else if (input[0] == *"w") {
-                printf("Write comming soon...\n");
+            bool slovo = false;
+            char argument[BUF_SIZE];
+            memset(argument, 0, BUF_SIZE);
+            char argumenty[ARGS_LIMIT][BUF_SIZE];
+            memset(argumenty, 0, ARGS_LIMIT*BUF_SIZE);
+            int pocet_pismen = 0;
+            int pocet_args = 0;
+            for (int i = 0; i < strlen(input); i++) {
+                if ((input[i] == *" " && slovo)) {
+                    strcpy(argumenty[pocet_args++], argument);
+                    memset(argument, 0, BUF_SIZE);
+                    pocet_pismen = 0;
+                    slovo = false;
+                }
+                if (input[i] != *" ") {
+                    slovo = true;
+                    argument[pocet_pismen++] = input[i];
+                }
+                if ((i + 1 == strlen(input)) && input[i] != *" ") {
+                    strcpy(argumenty[pocet_args++], argument);
+                    memset(argument, 0, BUF_SIZE);
+                    pocet_pismen = 0;
+                    slovo = false;
+                }
             }
+            if (!strcmp(argumenty[0], "a"))
+                append_mode = true;
+            else if (!strcmp(argumenty[0], "p"))
+                printf(file_buffer);
+            else if (!strcmp(argumenty[0], "w")) {
+                filename = argumenty[1];
+                if (!strcmp(argumenty[1], "") || strcmp(argumenty[2], ""))  {
+                    ERROR;
+                    continue;
+                }
+                file = fopen(filename, "w");
+                fprintf(file, "%s", file_buffer);
+                fclose(file);
+                printf("%d\n", pocet_radku);
+            }
+            else if (!strcmp(argumenty[0], "q"))
+                break;
+            else ERROR;
         } else if (!strcmp(input, ".")) {
             append_mode = false;
         } else {
